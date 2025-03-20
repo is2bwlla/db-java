79cdfc12c9eb0c275d2ed2784cdbb4e11fd57e1d
is2bwlla <lucyrose22198@gmail.com>
2025-03-20 16:50:36 -0300

    20-03  -- conectar toda a logica com o banco

--- src/main/java/br/ETS/almoxarifado/Main.java
+++ src/main/java/br/ETS/almoxarifado/Main.java
@@ -1,10 +1,104 @@
 package br.ETS.almoxarifado;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static ProdutoService produtoService = new ProdutoService();
+    private static Scanner scanner = new Scanner(System.in);
+
+    private static int exibirMenu() {
+        System.out.println("\n" +
+                "░█████╗░██╗░░░░░███╗░░░███╗░█████╗░██╗░░██╗░█████╗░██████╗░██╗███████╗░█████╗░██████╗░░█████╗░\n" +
+                "██╔══██╗██║░░░░░████╗░████║██╔══██╗╚██╗██╔╝██╔══██╗██╔══██╗██║██╔════╝██╔══██╗██╔══██╗██╔══██╗\n" +
+                "███████║██║░░░░░██╔████╔██║██║░░██║░╚███╔╝░███████║██████╔╝██║█████╗░░███████║██║░░██║██║░░██║\n" +
+                "██╔══██║██║░░░░░██║╚██╔╝██║██║░░██║░██╔██╗░██╔══██║██╔══██╗██║██╔══╝░░██╔══██║██║░░██║██║░░██║\n" +
+                "██║░░██║███████╗██║░╚═╝░██║╚█████╔╝██╔╝╚██╗██║░░██║██║░░██║██║██║░░░░░██║░░██║██████╔╝╚█████╔╝\n" +
+                "╚═╝░░╚═╝╚══════╝╚═╝░░░░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝╚═╝░░░░░╚═╝░░╚═╝╚═════╝░░╚════╝░\n" +
+                "\n" +
+                "███████╗████████╗░██████╗\n" +
+                "██╔════╝╚══██╔══╝██╔════╝\n" +
+                "█████╗░░░░░██║░░░╚█████╗░\n" +
+                "██╔══╝░░░░░██║░░░░╚═══██╗\n" +
+                "███████╗░░░██║░░░██████╔╝\n" +
+                "╚══════╝░░░╚═╝░░░╚═════╝░");
+
+        System.out.println("""
+                Selecione uma opção:
+                1) Inserir novo produto no almoxarifado
+                2) Listar produtos do almoxarifado
+                3) Adicionar determinada quantidade de um produto no almoxarifado
+                4) Remover determinada quantidade de um produto do almoxarifado
+                5) Remover um produto do almoxarifado
+                6) Encerrar a aplicação do almoxarifado
+                """);
+
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    private static void adicionarNovoProduto() {
+        System.out.println("Insira o ID do produto que deseja cadastrar:");
+        var id = Integer.parseInt(scanner.nextLine());
+        System.out.println("Insira o nome do produto que deseja cadastrar:");
+        var produto = scanner.nextLine();
+        System.out.println("Insira o partnumber do produto que deseja cadastrar:");
+        var parnumber = scanner.nextLine();
+        System.out.println("Insira a divisão do produto que deseja cadastrar:");
+        var divisao = scanner.nextLine();
+        System.out.println("Insira a quantidade desse produto:");
+        var quantidade = Integer.parseInt(scanner.nextLine());
+
+        produtoService.adicionarNovoProduto(new DadosProduto(id, produto, parnumber, divisao, quantidade));
+        System.out.printf("O produto %s foi cadastrado com sucesso\n", produto);
+        System.out.println("Pressione ENTER para voltar ao menu principal");
+        scanner.nextLine();
+    }
+
+    private static void exibirProdutosCadastrados() {
+        var produtos = produtoService.exibirProdutosDoAlmoxarifado();
+        produtos.forEach(System.out::println);
+    }
+
+    private static void adicionarQuantidadeDeUmProduto() {
+        System.out.println("Digite o id do produto que deseja adicionar:");
+        var id = Integer.parseInt(scanner.nextLine());
+        System.out.println("Digite a quantidade do produto que deseja adicionar:");
+        var quantidade = Integer.parseInt(scanner.nextLine());
+        produtoService.adicionarQuantidadeDeUmProduto(id, quantidade);
+        System.out.println("Pressione ENTER para voltar ao menu principal");
+        scanner.nextLine();
+    }
+
+    private static void removerQuantidadeDeUmProduto() {
+        System.out.println("Digite o ID do produto que deseja retirar:");
+        var id = Integer.parseInt(scanner.nextLine());
+        System.out.println("Digite a quantidade do produto que deseja retirar:");
+        var quantidade = Integer.parseInt(scanner.nextLine());
+        produtoService.removerQuantidadeDeUmProduto(id, quantidade);
+        System.out.println("Pressione ENTER para voltar ao menu principal");
+        scanner.nextLine();
+    }
+
+    private static void removerOProdutoDoAlmoxarifado() {
+        System.out.println("Digite o ID do produto que deseja remover do almoxarifado:");
+        var id = Integer.parseInt(scanner.nextLine());
+        produtoService.removerOProdutoDoAlmoxarifado(id);
+        System.out.println("Pressione ENTER para voltar ao menu principal");
+        scanner.nextLine();
+    }
+
     public static void main(String[] args) {
-        Produto produto = new Produto(new DadosProduto(1, "Bomba de combustivel", "459037A", "PS", 5));
-        System.out.println(produto.toString());
+        int opcao;
+        do {
+            opcao = exibirMenu();
+            switch (opcao) {
+                case 1 -> adicionarNovoProduto();
+                case 2 -> exibirProdutosCadastrados();
+                case 3 -> adicionarQuantidadeDeUmProduto();
+                case 4 -> removerQuantidadeDeUmProduto();
+                case 5 -> removerOProdutoDoAlmoxarifado();
+            }
+        } while (opcao != 6);
     }
 }
--- src/main/java/br/ETS/almoxarifado/ProdutoService.java
+++ src/main/java/br/ETS/almoxarifado/ProdutoService.java
@@ -13,4 +13,42 @@ public class ProdutoService {
         }
         produtos.add(produto);
     }
+
+    public ArrayList<Produto> exibirProdutosDoAlmoxarifado() {
+        return produtos;
+    }
+
+    public Produto encontrarProdutoPeloID(int id) {
+        return produtos
+                .stream() // Chama os métodos para manipulação de listas
+                .filter(produto -> produto.getId() == id) // filtra a lista com base em um critério, retorna lista filtrada
+                .findFirst() // Retorna o primeiro objeto da lista
+                .orElseThrow(() -> new RegraDaAplicacaoException("Produto com este ID não foi encontrado"));
+    }
+
+    public void adicionarQuantidadeDeUmProduto(int id, int quantidadeASerAdicionada) {
+        var produto = encontrarProdutoPeloID(id);
+        if(quantidadeASerAdicionada <= 0) {
+            throw new RegraDaAplicacaoException("Quantidade a ser adicionada deve ser maior que 0.");
+        }
+        produto.setQuantidade(produto.getQuantidade() + quantidadeASerAdicionada);
+    }
+
+    public void  removerQuantidadeDeUmProduto(int id, int quantidadeASerRemovida) {
+        var produto = encontrarProdutoPeloID(id);
+        if (quantidadeASerRemovida <= 0) {
+            throw new RegraDaAplicacaoException("Quantidade a ser removida deve ser maior que 0");
+        }
+
+        if (produto.getQuantidade() < quantidadeASerRemovida) {
+            throw new RegraDaAplicacaoException("Essa determinada quantidade não está disponível.");
+        }
+
+        produto.setQuantidade(produto.getQuantidade() - quantidadeASerRemovida);
+    }
+
+    public void removerOProdutoDoAlmoxarifado(int id) {
+        var produto = encontrarProdutoPeloID(id);
+        produtos.remove(produto);
+    }
 }
